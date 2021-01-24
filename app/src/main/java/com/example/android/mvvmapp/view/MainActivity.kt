bafab44a33c14211ed0a2f769b826f773f698050
Shoji Takayuki <sholv.gm8@gmail.com>
2021-01-24 15:13:35 +0900

    MainActivityとFragmentの作成とMainActivityとFragmentの接続

--- app/src/main/java/com/example/android/mvvmapp/view/MainActivity.kt
+++ app/src/main/java/com/example/android/mvvmapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android.mvvmapp
+package com.example.android.mvvmapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.mvvmapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
